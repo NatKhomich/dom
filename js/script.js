4c2e644b373e42aea875d64af973799de554eb21
NatKhomich <yalapulya94@mail.ru>
2023-02-06 16:54:52 +0600

    training

--- js/script.js
+++ js/script.js
@@ -15,4 +15,37 @@ console.log(bodyElement.hasChildNodes());
 const bodyChildren = bodyElement.children;
 console.log(bodyChildren);
 
+const bodyFirstElement = bodyElement.firstElementChild;
+console.log(bodyFirstElement);
+
+const bodyParentElement = bodyElement.parentElement;
+console.log(bodyParentElement);
+//поиск по селектору класса
+const oneElems = document.querySelectorAll(".lorem");
+console.log(oneElems);
+//поиск по селектору тега
+const twoElems = document.querySelectorAll("li");
+console.log(twoElems);
+//получ конкретного эл коллекции
+console.log(twoElems[1]);
+//создание эл-та
+const newElement = document.createElement("div");
+console.log(newElement);
+newElement.innerHTML = "Привет";
+//вставить эл
+const textElems = document.querySelector(".lorem"); //получ эл-т
+const newElementMy = document.createElement("div"); //созд новый эл
+newElementMy.innerHTML = "Новый элемент"; //наполняем эл
+textElems.before(newElementMy);
+//style
+const myElement = document.querySelector(".list");
+myElement.style.color = "blue";
+myElement.style.marginLeft = "40px";
+//перезапись стилей
+const myEl = document.querySelector(".lorem");
+myEl.style.cssText = `
+margin-top: 30px;
+margin-bottom: 30px;
+color: red;
+`;
 
