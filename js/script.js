ce30f199386c9b3b38edccfd8b42993e11bdb6ee
NatKhomich <yalapulya94@mail.ru>
2023-02-06 12:38:07 +0600

    workout

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const bodyElement = document.body;
+console.log(bodyElement);
+
+const firstChildNode = bodyElement.firstChild;
+const lastChildNode = bodyElement.lastChild;
+
+console.log(firstChildNode);
+console.log(lastChildNode);
+
+const childNodes = bodyElement.childNodes;
+console.log(childNodes);
+
+console.log(bodyElement.hasChildNodes());
+//
+const bodyChildren = bodyElement.children;
+console.log(bodyChildren);
