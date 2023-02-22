5a2f9fbe54cc4231c09df8bb4533da699e8a7566
NatKhomich <yalapulya94@mail.ru>
2023-02-22 12:08:43 +0600

    change DOM

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const div = document.createElement('div');
 div.classList.add('wrapper');
 //поместить его в тег body
 const tagBody = document.body//получаем body
-tagBody.appendChild(div);
+tagBody.append(div);
 console.log(tagBody)
 //создать заголовок h1 с текстом "DOM (Document Object Model)"
 const header = document.createElement('h1');
@@ -19,6 +19,44 @@ const ul = `
         <li>три</li>
     </ul>
 `;
+//поместить ul в div
+div.innerHTML = ul;
+//==================================================================
+//создать изображение
+const img = document.createElement('img');
+//добавить атрибут source
+img.src = 'https://picsum.photos/250';
+//добавить атрибут width сщ значением 250
+img.width = 250;
+//добавить класс super
+img.classList.add('super');
+//добавить свойство alt со значением "Super Man"
+img.alt = "Super Man";
+//поместить изображение внутрь div
+div.append(img);
+//используя HTML строку, сщздать div с классом pDiv с 2-мя параграфами
+const elemHTML = `
+    <div class = 'pDiv'>
+        <p>Параграф 1</p>
+        <p>Параграф 2</p>
+    </div>`;
+//поместить новый div до элемента <ul></ul>
+const ulList = div.querySelector('ul');
+ulList.insertAdjacentHTML('beforebegin', elemHTML)
+//добавить для 2-го параграфа класс текст
+const pDiv = document.querySelector('.pDiv');
+pDiv.children[1].classList.add('text');
+//удалить первый параграф
+pDiv.firstElementChild.remove();
+//====================================================================
+
+
+
+
+
+
+
+
 
 
 
